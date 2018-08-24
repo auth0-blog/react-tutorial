76f87b0f2909f44b68ec1aa77d79f2817f01a918
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-24 13:56:26 -0300

    navbar

--- qa-react/src/App.js
+++ qa-react/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar/>
         <p>Work in progress.</p>
       </div>
     );
---
+++ qa-react/src/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary fixed-top">
+      <Link className="navbar-brand" to="/">
+        Q&App
+      </Link>
+    </nav>
+  );
+}
+
+export default NavBar;
