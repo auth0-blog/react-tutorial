143709db46acd6b5477f616dd5d16ec4b1c0fb6c
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-24 13:54:35 -0300

    scaffolding the React app

---
+++ qa-react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Work in progress.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
