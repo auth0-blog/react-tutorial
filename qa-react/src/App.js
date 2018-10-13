84730b76cf395957a9dd3c42b7e2dc17a9b985cc
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-13 16:19:17 -0300

    show checking session state;

--- qa-react/src/App.js
+++ qa-react/src/App.js
@@ -9,6 +9,13 @@ import NewQuestion from './NewQuestion/NewQuestion';
 import SecuredRoute from './SecuredRoute/SecuredRoute';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    }
+  }
+
   async componentDidMount() {
     if (this.props.location.pathname === '/callback') return;
     try {
@@ -18,6 +25,7 @@ class App extends Component {
       if (err.error === 'login_required') return;
       console.log(err.error);
     }
+    this.setState({checkingSession:false});
   }
 
   render() {
@@ -27,7 +35,9 @@ class App extends Component {
         <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
         <Route exact path='/callback' component={Callback}/>
-        <SecuredRoute path='/new-question' component={NewQuestion} />
+        <SecuredRoute exact path='/new-question'
+                      component={NewQuestion}
+                      checkingSession={this.state.checkingSession} />
       </div>
     );
   }
--- qa-react/src/SecuredRoute/SecuredRoute.js
+++ qa-react/src/SecuredRoute/SecuredRoute.js
@@ -3,9 +3,10 @@ import {Route} from 'react-router-dom';
 import auth0Client from '../Auth';
 
 function SecuredRoute(props) {
-  const {component: Component, path} = props;
+  const {component: Component, path, checkingSession} = props;
   return (
     <Route path={path} render={() => {
+      if (checkingSession) return <h3 className="text-center">Validating session...</h3>;
       if (!auth0Client.isAuthenticated()) {
         auth0Client.signIn();
         return <div />;
